7e1a8417020e551a1f680844bbcb6578c4906227
uJZk <34388941+uJZk@users.noreply.github.com>
2023-07-06 00:15:57 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -91,7 +91,6 @@ export kernel_usage="stable"
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 # openwrt-x86-64-generic-squashfs-rootfs.img.gz
 cat >"$CLEAR_PATH" <<-EOF
-07.05-Lede-18.06-6.1.35-x86-64-generic-squashfs-combined.img.gz
 ipk.tar.gz
 packages
 config.buildinfo
